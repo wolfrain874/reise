fed5e959635b85886c0be96ca12c6679da7c6285
mau1015 <74670802+mauricio1111111@users.noreply.github.com>
2022-06-29 14:25:09 -0500

    intento hacer un buscador

--- js/script.js
+++ js/script.js
@@ -27,3 +27,31 @@ var swiper = new Swiper(".slide-content", {
         },
     },
   });
+   
+  const productos = [
+    {nombre: hotel1, valor: 1111, lugar:napoles},
+    {nombre: hotel2, valor: 1111, lugar:napoles},
+    {nombre: hotel3, valor: 1111, lugar:napoles},
+    {nombre: hotel4, valor: 1111, lugar:napoles},
+  ]
+
+  const formulario=document.querySelector('#formulario');
+  const boton=document.querySelector('#boton');
+  const resultado = document.querySelector('#resultado');
+  const filtrar=()=>{
+  //console.log(formulario.value);
+  resultado.innerHTML='';
+  const texto=formulario.value.toLowerCase();
+  for(let producto of productos){
+    let nombre = producto.nombre.toLowerCase();
+    if(nombre.indexOf(texto) !== 1){
+      resultado.innerHTML+=`<li>${producto.nombre}</li> -valor: ${producto.valor} -lugar: ${producto.lugar}` ;
+
+    }
+
+  }
+  if(resultado.innerHTML===''){
+    resultado.innerHTML +='<li>No se encontraron resultados</li>';
+  }
+}
+boton.addEventListener('click',filtrar)
